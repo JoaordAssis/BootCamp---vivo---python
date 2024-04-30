c3feb25df62f40385fbc39361ea65eebf7ba2512
JoaordAssis <joaocr.assis@gmail.com>
2024-04-30 19:22:47 -0300

    Adicionando todos os arquivos modificados/movidos

---
+++ Python/exercicios.py
@@ -0,0 +1,19 @@
+# EX: 1
+
+# n = int(input("Digite um número:"))
+
+# contador = 0
+
+
+# print(type(n),type(contador))
+
+# while(contador < n):
+#     contador += 1
+#     print(f"A soma de {n} e {contador} é de {n + contador}")
+
+
+
+#EX: 2
+
+for i in range(0, 1001):
+    print(i)
---
+++ Python/primeiro_programa.py
@@ -0,0 +1,5 @@
+print("Hello Python World!\n"*5)
+
+print(1+1)
+
+print(float(1))
